1001bf1a273e1e0bba8f64340d872164ac7edbd7
Marina-dzh <106904728+Marina-dzh@users.noreply.github.com>
2022-11-14 19:02:18 +0000

    Deploying to gh-pages from @ Marina-dzh/goit-js-hw-09@188d7439ea7db852d758de99eb7d9854c7afe4a0 🚀

---
+++ 01-color-switcher.873a1eed.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")},e=null;t.start.addEventListener("click",(function(){t.start.disabled=!0,e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.873a1eed.js.map
